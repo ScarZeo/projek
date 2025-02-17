15648df75dde3e7c43e6ebe374a8d8f09adffedb
ScarZeo <123240204@student.upnyk.ac.id>
2025-02-17 14:20:06 +0700

    Add files via upload

---
+++ projekpraktikum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+struct datasiswa
+{
+    int nis;
+    string nama;
+    int kelas;
+    int absen;
+};
+datasiswa siswa[99];
+
+main()
+{
+    cout<<"";
+}
